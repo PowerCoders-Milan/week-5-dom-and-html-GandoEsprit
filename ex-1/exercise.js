1d598fb2a4c74d9b2963467e9963d89ba1fcab03
gando diallo <gandoesprit@gmail.com>
2021-03-02 11:40:34 +0100

    Solution to exercrse 1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,8 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
-
+    var elem = document.querySelectorAll('p');
+    elem.forEach(function(el){
+        el.style.backgroundColor = "blue";
+    })
 }
