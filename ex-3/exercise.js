8633ae4a8a4dcc12e7b26b0b454929e142794887
gando diallo <gandoesprit@gmail.com>
2021-03-02 15:46:31 +0100

    Solution to exercice 3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,11 @@
 function insert_Row() {
+    var table = document.getElementById('sampleTable');
+    var tr = table.insertRow();
+    var td1 = tr.insertCell();
+    var txtCellul = document.createTextNode('New Cell1')
+    td1.appendChild(txtCellul);
+    var td2 = tr.insertCell();
+    txtCellul = document.createTextNode('New Cell2');
+    td2.appendChild(txtCellul);
 
 }
